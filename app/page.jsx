998b6341656a13bd7edbdcc3e42ab6a8fab8eb76
QueirogaAmorim <queiroga.fernandes@hotmail.com>
2025-02-04 22:25:01 +0000

    Remove footer

--- app/page.jsx
+++ app/page.jsx
@@ -35,14 +35,25 @@ export default function Page() {
                 <h1 className="mb-0">Dragon Ball X</h1>
                 <h3 className="mb-0">The War for Power</h3>
             </section>
-            <Image
+            <section>
+                <p>
+                Em um universo paralelo, onde a paz alcançada pelas lendas de Goku e seus aliados ainda ecoa, novas ameaças surgem do vazio cósmico. Dragon Ball X - The War for Power nos transporta para uma batalha épica entre forças desconhecidas, mais poderosas e traiçoeiras do que qualquer vilão que a Terra já enfrentou.
+
+Após a derrota de grandes inimigos, como Freeza e Majin Buu, os heróis Z acreditavam que poderiam viver tranquilos. Porém, uma misteriosa distorção no espaço-tempo traz à tona uma guerra interdimensional entre exércitos de poderosos guerreiros e antigos deuses esquecidos. O maior desafio da história de Dragon Ball começa quando um novo guerreiro, com habilidades além da imaginação, ameaça desestabilizar as fronteiras entre os universos e roubar a energia vital dos próprios Deuses.
+
+Goku, Vegeta, e seus amigos se veem arrastados para uma guerra sem precedentes, onde não só suas vidas estão em risco, mas também o equilíbrio de todos os cosmos. Velhos aliados ressurgem, novos personagens entram em cena, e o poder nunca visto antes será forjado em batalha. O destino da guerra está nas mãos de heróis e vilões que desejam, acima de tudo, conquistar o poder supremo.
+
+Prepare-se para um confronto épico e imersivo! Dragon Ball X - The War for Power está prestes a começar.
+                </p>
+            </section>
+            {/* <Image
                                     src="/images/logo-large.jpeg"
                                     priority
                                     fill={true}
                                     style={{ objectFit: 'contain' }}
                                     sizes="(max-width: 1024px) 100vw, 1024px"
                                     alt="DB X"
-                                />
+                                /> */}
         </main>
     );
 }
