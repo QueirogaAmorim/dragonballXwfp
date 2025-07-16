5b2894e9ef769fe1f5c92825f7d704aaf83f5ba7
QueirogaAmorim <queiroga.fernandes@hotmail.com>
2025-07-16 08:22:46 +0100

    Update

--- app/page.jsx
+++ app/page.jsx
@@ -37,11 +37,11 @@ export default function Page() {
             </section>
             <section>
                 <p>
-                Em um universo paralelo, onde a paz alcançada pelas lendas de Goku e seus aliados ainda ecoa, novas ameaças surgem do vazio cósmico. Dragon Ball X - The War for Power nos transporta para uma batalha épica entre forças desconhecidas, mais poderosas e traiçoeiras do que qualquer vilão que a Terra já enfrentou.
+                Em um universo paralelo, onde a paz alcançada pelas lendas de Goku e seus aliados ainda ecoa, novas ameaças surgem do vazio cósmico. Dragon Ball X - The War for Power transporta-nos para uma batalha épica entre forças desconhecidas, mais poderosas e traiçoeiras do que qualquer vilão que a Terra já enfrentou.
 
 Após a derrota de grandes inimigos, como Freeza e Majin Buu, os heróis Z acreditavam que poderiam viver tranquilos. Porém, uma misteriosa distorção no espaço-tempo traz à tona uma guerra interdimensional entre exércitos de poderosos guerreiros e antigos deuses esquecidos. O maior desafio da história de Dragon Ball começa quando um novo guerreiro, com habilidades além da imaginação, ameaça desestabilizar as fronteiras entre os universos e roubar a energia vital dos próprios Deuses.
 
-Goku, Vegeta, e seus amigos se veem arrastados para uma guerra sem precedentes, onde não só suas vidas estão em risco, mas também o equilíbrio de todos os cosmos. Velhos aliados ressurgem, novos personagens entram em cena, e o poder nunca visto antes será forjado em batalha. O destino da guerra está nas mãos de heróis e vilões que desejam, acima de tudo, conquistar o poder supremo.
+Goku, Vegeta, e os seus amigos são arrastados para uma guerra sem precedentes, onde não só as suas vidas estão em risco, mas também o equilíbrio de todos o cosmos. Velhos aliados ressurgem, novas personagens entram em cena, e o poder nunca antes visto será forjado em batalha. O destino da guerra está nas mãos de heróis e vilões que desejam, acima de tudo, conquistar o poder supremo.
 
 Prepare-se para um confronto épico e imersivo! Dragon Ball X - The War for Power está prestes a começar.
                 </p>
